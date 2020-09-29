fb1e4887e358f7eb05b9dbaf3928f36b9708c706
hotclimbing <zzerocks@gmail.com>
2020-09-29 18:57:09 +0300

    Удаление моделей из проекта. View через render

--- tours/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class DummyModel(models.Model):
-    pass
--- tours/views.py
+++ tours/views.py
@@ -1,17 +1,17 @@
-from django.views.generic import ListView
-from tours.models import DummyModel
+from django.shortcuts import render
+from django.views import View
 
 
-class MainView(ListView):
-    template_name = 'tours/index.html'
-    model = DummyModel
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request=request, template_name='tours/index.html')
 
 
-class DepartureView(ListView):
-    template_name = 'tours/departure.html'
-    model = DummyModel
+class DepartureView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request=request, template_name='tours/departure.html')
 
 
-class TourView(ListView):
-    template_name = 'tours/tour.html'
-    model = DummyModel
+class TourView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request=request, template_name='tours/tour.html')
