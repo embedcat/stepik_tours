5290bbda71a3e8cfa32ff0161382bdb79ccc02d7
hotclimbing <zzerocks@gmail.com>
2020-10-01 21:02:20 +0300

    Add 404 and 500 errors handlers

--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
@@ -15,3 +16,11 @@ class DepartureView(View):
 class TourView(View):
     def get(self, request, *args, **kwargs):
         return render(request=request, template_name='tours/tour.html')
+
+
+def page_not_found_view(request, exception):
+    return HttpResponse("Page not found!")
+
+
+def error_view(request):
+    return HttpResponse("Server error!")
